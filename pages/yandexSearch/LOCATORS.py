d23866d24751d8d9377cba3a2455e5e8963eef01
LenaRedkinale <lenochkar35@mail.ru>
2022-10-02 16:37:13 +0300

    Добавлен локатор  для поиска клавиатуры

---
+++ pages/yandexSearch/LOCATORS.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+class Locators:
+    search_field = (By.ID, "text")
+    search_button = (By.CLASS_NAME, "search3__button.mini-suggest__button")
+    cancel_button = (By.CLASS_NAME, "search3__svg_clear")
+    keyboard_button = (By.CLASS_NAME, "search3__svg_keyboard")
